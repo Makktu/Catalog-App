29118f406462e351a5fbfca57489d82c1806374e
Makktu <johnmcna@gmail.com>
2023-12-07 03:57:27 +0000

    setting up dummy data

---
+++ data/test-data.js
@@ -0,0 +1,39 @@
+export default TEST_DATA = [
+  {
+    title: 'SundayRoadLike',
+    type: 'prose fiction',
+    started: '8 February 2023',
+    lastTouched: '6 December 2023',
+    wordCount: 16000,
+    viability: 'B',
+    archived: false,
+    summaryShort: 'A McCarthy-like effort',
+    summaryLong:
+      'Some promising signs of craft in this McCarthy-Kafka-Dostoevsky knockoff',
+  },
+  {
+    title: 'Boxing Day Tethered',
+    type: 'prose fiction',
+    started: '23 January 2023',
+    lastTouched: '3 December 2023',
+    wordCount: 20955,
+    viability: 'C',
+    archived: false,
+    summaryShort: 'An instalment in RoR/Tethered',
+    summaryLong:
+      'The Couriers are all Constructs, and they know they are. The others want to Rebel against their assigned fates. Courier J has to be persuaded to join in. Slavery is a risky theme in this one.',
+  },
+  {
+    title: 'Journal of the Plague Months',
+    type: 'prose non-fiction',
+    started: '6 March 2020',
+    lastTouched: '31 August 2020',
+    wordCount: 24332,
+    viability: 'E',
+    archived: false,
+    summaryShort:
+      'An account of living in the first weeks of the coronavirus pandemic of 2020.',
+    summaryLong:
+      "A lone individual's record of life in the first few weeks and months of the coronavirus pandemic of the year 2020.",
+  },
+];
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-'use strict';
+import { TEST_DATA } from './data/test-data';
 
-console.log('working');
+console.log(TEST_DATA[0].title, TEST_DATA[0].viability);
+console.log('hi');
